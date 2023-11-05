8b016b0075e2674ca8a705dfd983710ac8e28b01
baegofda <molly.daleo3o3@gmail.com>
2023-11-06 02:20:00 +0900

    chore: 파일 절대경로 세팅 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          src: './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
